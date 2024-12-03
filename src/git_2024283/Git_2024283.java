393472645a3f3f802af4af5d333a5a086433876e
lubum593 <luis.mb593@gmail.com>
2024-12-03 00:04:49 +0000

    I just added a simple print.

--- src/git_2024283/Git_2024283.java
+++ src/git_2024283/Git_2024283.java
@@ -15,6 +15,7 @@ public class Git_2024283 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Luis - 2024283");        
     }
     
 }
